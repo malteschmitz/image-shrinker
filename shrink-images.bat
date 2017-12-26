f47524d057f4b577ecec69b39d66095369c7b8f5
Malte Schmitz <malte@schmitz-sh.de>
2017-12-26 20:46:47 +0100

    Create shrink-images.bat

---
+++ shrink-images.bat
@@ -0,0 +1,27 @@
+@echo off
+
+set outdir=C:\Users\em\Downloads\kleine-fotos
+set suffix=_klein
+set size=1024
+set quality=95
+
+setlocal ENABLEDELAYEDEXPANSION
+rem *** Take the cmd-line, remove all until the first parameter
+rem *** Copy cmdcmdline without any modifications, as cmdcmdline has some strange behaviour
+set "params=!cmdcmdline!"
+set "params=!params:~0,-1!"
+set "params=!params:*" =!"
+
+rem create outdir if not yet existing
+if not exist %outdir% mkdir %outdir%
+
+rem Split the parameters on spaces but respect the quotes
+for %%G IN (!params!) do (
+  magick convert "%%~G" -resize %size%x%size% -quality %quality% "%outdir%\%%~nG%suffix%%%~xG"
+)
+
+start %outdir%
+pause
+
+REM ** The exit is important, so the cmd.exe doesn't try to execute commands after ampersands
+exit
